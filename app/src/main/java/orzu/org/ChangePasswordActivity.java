19d333d6cbf26b17ce19eaf7a6fa079f4bb9e1e6
QuietsT <android@orzu.me>
2019-10-15 15:14:55 +0600

    Maket Change pass

--- app/src/main/java/orzu/org/ChangePasswordActivity.java
+++ app/src/main/java/orzu/org/ChangePasswordActivity.java
@@ -1,36 +1,50 @@
 package orzu.org;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ChangePasswordActivity extends AppCompatActivity implements View.OnClickListener {
     ProgressBar bar;
     EditText oldPwd, newPwd, newPwdRe;
-    Button ok;
+    FloatingActionButton ok;
+    ImageView back;
+    CardView cardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getSupportActionBar().hide();
+        getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryOrangeTop));
         setContentView(R.layout.activity_change_password);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient_back));
-        getSupportActionBar().setTitle("Изменить пароль");
-        getSupportActionBar().setElevation(0);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+//        getSupportActionBar().setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient_back));
+//        getSupportActionBar().setTitle("Изменить пароль");
+//        getSupportActionBar().setElevation(0);
 
         bar = findViewById(R.id.progressBarLogin_reg);
+        back = findViewById(R.id.change_back);
+        cardView = findViewById(R.id.constrlog2);
+        cardView.setBackgroundResource(R.drawable.shape_card_topcorners);
         oldPwd = findViewById(R.id.old_pwd_edit);
         newPwd = findViewById(R.id.new_pwd_edit);
         newPwdRe = findViewById(R.id.new_pwd_edit_rewrite);
         ok = findViewById(R.id.button_change_pwd);
         ok.setOnClickListener(this);
+        back.setOnClickListener(this);
 
         bar.setVisibility(View.GONE);
 
@@ -38,10 +52,17 @@ public class ChangePasswordActivity extends AppCompatActivity implements View.On
 
     @Override
     public void onClick(View view) {
-        bar.setVisibility(View.VISIBLE);
-        if (!newPwd.getText().toString().equals(newPwdRe.getText().toString())) {
-            Toast.makeText(this, "Пароли не совпадают "+newPwd.getText()+" "+newPwdRe.getText(), Toast.LENGTH_SHORT).show();
-            bar.setVisibility(View.GONE);
+        switch (view.getId()){
+            case R.id.change_back:
+                finish();
+                break;
+            case R.id.button_change_pwd:
+                bar.setVisibility(View.VISIBLE);
+                if (!newPwd.getText().toString().equals(newPwdRe.getText().toString())) {
+                    Toast.makeText(this, "Пароли не совпадают "+newPwd.getText()+" "+newPwdRe.getText(), Toast.LENGTH_SHORT).show();
+                    bar.setVisibility(View.GONE);
+                }
+                break;
         }
     }
 
