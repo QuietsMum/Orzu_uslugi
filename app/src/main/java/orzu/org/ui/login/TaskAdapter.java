f5a84f4dfa1d5ac5e116076c03184b6dab2c4748
Mas9nn <almasnurlanov16@gmail.com>
2019-10-25 12:45:25 +0600

    Registration

--- app/src/main/java/orzu/org/ui/login/TaskAdapter.java
+++ app/src/main/java/orzu/org/ui/login/TaskAdapter.java
@@ -111,7 +111,7 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
 
     private void chooseSuggester(Object id) {
 
-        String url = "https://orzu.org/api?appid=$2y$12$esyosghhXSh6LxcX17N/suiqeJGJq/VQ9QkbqvImtE4JMWxz7WqYS" +
+        String url = "https://projectapi.pw/api?appid=$2y$12$esyosghhXSh6LxcX17N/suiqeJGJq/VQ9QkbqvImtE4JMWxz7WqYS" +
                 "&opt=task_requests" +
                 "&act=selected" +
                 "&req_id=" + id +
