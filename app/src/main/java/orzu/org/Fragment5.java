0fa47fcbea888526761042ac4edb3733de2594e3
Mas9nn <almasnurlanov16@gmail.com>
2019-12-06 10:49:50 +0600

    fragment5

--- app/src/main/java/orzu/org/Fragment5.java
+++ app/src/main/java/orzu/org/Fragment5.java
@@ -1,26 +1,37 @@
 package orzu.org;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Fragment5 extends Fragment {
+public class Fragment5 extends Fragment implements View.OnClickListener {
 
     RecyclerView bonus_recycler;
     List<Bonuses> bonuses = new ArrayList<>();
     CardView cardView;
+    TextView bonus_left,bonus_right;
+    ImageView tri_left,tri_right,qr_code;
+    ConstraintLayout bonus_left_const,bonus_right_const;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -28,6 +39,15 @@ public class Fragment5 extends Fragment {
 
         bonus_recycler = v.findViewById(R.id.bonus_recycler);
         cardView = v.findViewById(R.id.card_of_partner);
+        bonus_left = v.findViewById(R.id.bonus_left);
+        bonus_right = v.findViewById(R.id.bonus_right);
+        tri_left = v.findViewById(R.id.bonus_tri_left);
+        tri_right = v.findViewById(R.id.bonus_tri_right);
+        qr_code = v.findViewById(R.id.qr_code);
+        bonus_left_const = v.findViewById(R.id.bonus_left_const);
+        bonus_right_const = v.findViewById(R.id.bonus_right_const);
+        bonus_left.setOnClickListener(this);
+        bonus_right.setOnClickListener(this);
         cardView.setBackgroundResource(R.drawable.shape_card_topcorners);
         bonus_recycler.setHasFixedSize(true);
         bonus_recycler.setNestedScrollingEnabled(false);
@@ -49,6 +69,32 @@ public class Fragment5 extends Fragment {
 
             }
         });
+        BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+        Bitmap bitmap = null;
+        try {
+            bitmap = barcodeEncoder.encodeBitmap("HelloMazafaka", BarcodeFormat.QR_CODE, 400, 400);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+        qr_code.setImageBitmap(bitmap);
         return v;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bonus_left:
+                tri_right.setVisibility(View.INVISIBLE);
+                tri_left.setVisibility(View.VISIBLE);
+                bonus_left_const.setVisibility(View.VISIBLE);
+                bonus_right_const.setVisibility(View.GONE);
+                break;
+            case R.id.bonus_right:
+                tri_left.setVisibility(View.INVISIBLE);
+                tri_right.setVisibility(View.VISIBLE);
+                bonus_left_const.setVisibility(View.GONE);
+                bonus_right_const.setVisibility(View.VISIBLE);
+                break;
+        }
     }
 }
